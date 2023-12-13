1b4e6a1a924f809cc94bc2bcbfd7d8ae90fbd8e4
despaqui9u <liamdespaqiis@gmail.com>
2023-12-13 17:28:23 +0100

    Bilan_Ité1

--- src/main/java/trellol/trellol/Historique.java
+++ src/main/java/trellol/trellol/Historique.java
@@ -5,12 +5,26 @@ import java.util.ArrayList;
 public class Historique {
     private ArrayList<String> actions;
 
+    public static String DEPLACEMENT_ACTION = "La tâche {objet} a été déplacé";
+
+    public static String SUPRESSION_ACTION = "La tâche {objet} a été supprimé";
+
+    public static String ARCHIVAGE_ACTION = "La tâche {objet} a été archivé";
+
+    public static String DESARCHIVAGE_ACTION = "La tâche {objet} a été désarchivé";
+
+    public static String CREATION_TACHE_ACTION = "La tâche {objet} a été créé";
+
+    public static String CREATION_COLONNE = "La colonne {objet} a été créé";
+
+    public static String EXPORTER_TABLEAU = "Le tableau {objet} a été exporté";
+
     public Historique(){
         this.actions = new ArrayList<String>();
     }
 
-    public void addAction(String action){
-        this.actions.add(action);
+    public void addAction(String action,String objet){
+        this.actions.add(action.replace("{objet}",objet));
     }
 
     public ArrayList<String> getActions(){
