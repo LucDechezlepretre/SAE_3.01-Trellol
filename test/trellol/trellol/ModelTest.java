99152c7cb9fe7787753e31e0e44aa3b05566b38d
RobinSlimani <RobinSlimani>
2023-12-15 15:16:57 +0100

    ajout before each dans les tests

--- test/trellol/trellol/ModelTest.java
+++ test/trellol/trellol/ModelTest.java
@@ -1,5 +1,6 @@
 package trellol.trellol;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,13 +9,18 @@ class ModelTest {
     /**
      * vérification de l'archivage d'une tache
      */
+    Model m;
+    @BeforeEach
+    void setUp() {
+        m = new Model();
+    }
+
     @Test
     void archiverTache() {
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "Bonjour", 1);
         m.ajouterTache(t);
         m.archiverTache(t);
-        assertEquals(t.getEtat(), Tache.ETAT_ARCHIVE, "La tache devrait etre archivée");
+        assertEquals(Tache.ETAT_ARCHIVE, t.getEtat(),"La tache devrait etre archivée");
     }
 
     /**
@@ -22,13 +28,12 @@ class ModelTest {
      */
     @Test
     void desarchiverTache() {
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "Bonjour", 1);
         m.ajouterTache(t);
         m.archiverTache(t);
-        assertEquals(t.getEtat(), Tache.ETAT_ARCHIVE, "La tache devrait etre archivée");
+        assertEquals(Tache.ETAT_ARCHIVE, t.getEtat(), "La tache devrait etre archivée");
         m.desarchiverTache(t);
-        assertEquals(t.getEtat(), Tache.ETAT_NON_ARCHIVE, "La tache devrait etre desarchivée");
+        assertEquals(Tache.ETAT_NON_ARCHIVE, t.getEtat(), "La tache devrait etre desarchivée");
     }
 
     /**
@@ -36,11 +41,10 @@ class ModelTest {
      */
     @Test
     void suppressionTache() {
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "Bonjour", 1);
         m.ajouterTache(t);
         m.suppressionTache(t);
-        assertEquals(t.getEtat(), Tache.ETAT_SUPPRIME, "La tache devrait etre supprimée");
+        assertEquals(Tache.ETAT_SUPPRIME, t.getEtat(),"La tache devrait etre supprimée");
     }
 
     /**
@@ -48,7 +52,6 @@ class ModelTest {
      */
     @Test
     void deplacerTache() {
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "enfant", 1);
         Tache t2 = new Tache("tache2", "13/12/2023", 2, "parent", 1);
         Tache t3 = new Tache("tache3", "13/12/2023", 2, "enfant2", 1);
@@ -60,7 +63,7 @@ class ModelTest {
         m.ajouterTache(t3);
         //on déplace la tache t2 pour qu'elle devienne enfant de t
         m.deplacerTache(t2, t);
-        assertEquals(t2.getParent(), t, "La tache devrait etre déplacée");
+        assertEquals(t, t2.getParent(),"La tache devrait etre déplacée");
     }
 
 
@@ -70,26 +73,24 @@ class ModelTest {
      */
     @Test
     public void testAjouterTache(){
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "Bonjour", 1);
 
         //ajout racine
         m.ajouterTache(t);
-        assertEquals(m.getEnsTache().size(), 1, "La tache devrait etre ajoutée");
+        assertEquals(1,m.getEnsTache().size(), "La tache devrait etre ajoutée");
 
         //ajout tache sans parent
         Tache t2 = new Tache("tache2", "13/12/2023", 2, "Bonjour", 1);
         m.ajouterTache(t2);
-        assertEquals(m.getEnsTache().size(), 1, "La tache ne devrait pas etre ajoutée");
+        assertEquals(1,m.getEnsTache().size(), "La tache ne devrait pas etre ajoutée");
         t2.setParent(t);
 
         //ajout tache avec parent
         m.ajouterTache(t2);
-        assertEquals(m.getEnsTache().size(), 2, "La tache devrait etre ajoutée");
+        assertEquals(2,m.getEnsTache().size(), "La tache devrait etre ajoutée");
     }
     @Test
     public void testGetRacine(){
-        Model m = new Model();
         Tache t = new Tache("tache1", "13/12/2023", 2, "Bonjour", 1);
         m.ajouterTache(t);
         assertEquals(t, m.getRacine(), "Les deux tâches devraient être identiques");
