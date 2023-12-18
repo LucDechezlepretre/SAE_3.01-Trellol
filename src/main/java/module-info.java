0ac81904a14209c8f9a7eadc33400bb7d951eccc
Akip2 <126561427+Akip2@users.noreply.github.com>
2023-12-18 16:52:15 +0100

    Début du

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module trellol.trellol {
     requires javafx.controls;
+    requires java.sql;
     exports trellol.trellol;
     exports trellol.trellol.Modele;
     exports trellol.trellol.Vues;
--- src/main/java/trellol/trellol/Controleurs/ControlleurAjouterTache.java
+++ src/main/java/trellol/trellol/Controleurs/ControlleurAjouterTache.java
@@ -6,8 +6,13 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import trellol.trellol.Importance;
 import trellol.trellol.Modele.Model;
+import trellol.trellol.Tache;
 import trellol.trellol.Vues.Affichage;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class ControlleurAjouterTache implements EventHandler<ActionEvent> {
     private Model modele;
@@ -30,5 +35,28 @@ public class ControlleurAjouterTache implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent actionEvent) {
+        //Creation de la tache
+        String nom=this.nom.getText(); //NOM DE LA TACHE
+
+        String dateString=this.date.toString(); //DATE DEBUT
+
+        int duree=Integer.parseInt(this.duree.getText()); //DUREE
+
+        String description=this.description.getText(); //DESCRIPTION
+
+        String libelImp=this.importance.getValue();  //IMPORTANCE
+        int importance;
+        if(libelImp.equals("faible")){
+            importance= Importance.FAIBLE;
+        }
+        else if(libelImp.equals("moyenne")){
+            importance=Importance.MOYENNE;
+        }
+        else{
+            importance=Importance.ELEVEE;
+        }
+
+        Tache tache=new Tache(nom, dateString, duree,description, importance);
+        this.modele.ajouterTache(tache);
     }
 }
