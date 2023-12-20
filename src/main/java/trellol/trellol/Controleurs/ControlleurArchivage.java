dc1fabdae1c5eec66e55b90f47933275eedbeea0
Akip2 <126561427+Akip2@users.noreply.github.com>
2023-12-20 16:44:18 +0100

    Ajout du controlleur du bouton d'archivage

--- src/main/java/trellol/trellol/Controleurs/ControlleurArchivage.java
+++ src/main/java/trellol/trellol/Controleurs/ControlleurArchivage.java
@@ -7,8 +7,12 @@ import trellol.trellol.Tache;
 
 public class ControlleurArchivage implements EventHandler {
     private Modele m;
-
     private Tache tache;
+
+    public ControlleurArchivage(Modele m, Tache t){
+        this.m=m;
+        this.tache=t;
+    }
 
     @Override
     public void handle(Event event) {
