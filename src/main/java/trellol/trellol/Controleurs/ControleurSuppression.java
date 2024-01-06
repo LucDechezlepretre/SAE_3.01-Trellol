55e40b9d352cbc6f0b8bbf023455e935587d1839
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 15:07:54 +0100

    ajout de javadoc pour ControleurSuppression

--- src/main/java/trellol/trellol/Controleurs/ControleurSuppression.java
+++ src/main/java/trellol/trellol/Controleurs/ControleurSuppression.java
@@ -7,12 +7,27 @@ import trellol.trellol.Modele.Modele;
 import trellol.trellol.Tache;
 import trellol.trellol.Vues.MainAffichage;
 
+/**
+ * Classe controleur pour la suppression d'une tache
+ */
 public class ControleurSuppression implements EventHandler {
+    /**
+     * Attribut m de type Modele
+     */
     private Modele m;
 
     private Stage fenetre;
+    /**
+     * Attribut tache de type Tache représentant la tâche à supprimer
+     */
     private Tache tache;
 
+    /**
+     * Constructeur pour le controleur
+     * @param m le modele
+     * @param fenetre la fenetre si le controleur est dans la fenetre de modification d'une tache
+     * @param t
+     */
     public ControleurSuppression(Modele m, Stage fenetre, Tache t){
         this.m=m;
         this.fenetre=fenetre;
@@ -25,6 +40,11 @@ public class ControleurSuppression implements EventHandler {
         this.tache=t;
     }
 
+    /**
+     * Redéfinition de la méthode handle, appelé lors du "clique"
+     * sur le bouton supprimer
+     * @param event évènement sur le bouton associé
+     */
     @Override
     public void handle(Event event) {
         this.m.suppressionTache(this.tache);
