b73709d897e3f03f47b60db158cacd8a9bde218d
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 15:44:58 +0100

    ajout de javadoc pour Sujet

--- src/main/java/trellol/trellol/Modele/Sujet.java
+++ src/main/java/trellol/trellol/Modele/Sujet.java
@@ -2,8 +2,22 @@ package trellol.trellol.Modele;
 
 import trellol.trellol.Vues.Observateur;
 
+/**
+ * Interface devant être implémenté par chaque modèle de l'application,
+ * car elle permet une gestion conforme au patron MVC des observateurs
+ */
 public interface Sujet {
+        /**
+         * Ajoute un observateur au modèle
+         */
         public void enregistrerObservateur(Observateur o);
+        /**
+         * Supprime un observateur au modèle
+         */
         public void supprimerObservateur(Observateur o);
+        /**
+         * Informe tous les observateurs du modèle des
+         * modifications 
+         */
         public void notifierObservateurs();
 }
