6a6fd67a4834b451d6c656460a6fabde5e60c92e
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 19:09:43 +0100

    ajout de javadoc pour Historique

--- src/main/java/trellol/trellol/Historique.java
+++ src/main/java/trellol/trellol/Historique.java
@@ -3,35 +3,68 @@ package trellol.trellol;
 import java.util.ArrayList;
 
 public class Historique {
+    /**
+     * Attribut liste représentant la totalité des actions
+     */
     private ArrayList<String> actions;
 
+    /**
+     * Attribut String représentant l'action de modification d'une tâche
+     */
     public static String MODIFICATION_ACTION = "La tâche {objet} a été modifiée";
+    /**
+     * Attribut String représentant l'action de déplacement d'une tâche
+     */
     public static String DEPLACEMENT_ACTION = "La tâche {objet} a été déplacé";
-
+    /**
+     * Attribut String représentant l'action de suppression d'une tâche
+     */
     public static String SUPRESSION_ACTION = "La tâche {objet} a été supprimé";
-
+    /**
+     * Attribut String représentant l'action d'archivage d'une tâche
+     */
     public static String ARCHIVAGE_ACTION = "La tâche {objet} a été archivé";
-
+    /**
+     * Attribut String représentant l'action de déarchivage d'une tâche
+     */
     public static String DESARCHIVAGE_ACTION = "La tâche {objet} a été désarchivé";
-
+    /**
+     * Attribut String représentant l'action de la création d'une tâche
+     */
     public static String CREATION_TACHE_ACTION = "La tâche {objet} a été créé";
-
-    public static String CREATION_COLONNE = "La colonne {objet} a été créé";
-
+    /**
+     * Attribut String représentant l'action d'exportation des tâches dans un fichier
+     */
     public static String EXPORTER_TABLEAU = "Le tableau {objet} a été exporté";
 
+    /**
+     * Constructeur de l'historique
+     */
     public Historique(){
         this.actions = new ArrayList<String>();
     }
 
+    /**
+     * Méthode permettant l'ajout d'une action dans l'historique
+     * @param action action qui sera ajoutée à l'historique
+     * @param objet nom de l'objet qui subit l'action
+     */
     public void addAction(String action,String objet){
         this.actions.add(action.replace("{objet}",objet));
     }
 
+    /**
+     * Getter de l'attribut qui contient toutes les actions
+     * @return la liste des actions commises
+     */
     public ArrayList<String> getActions(){
         return this.actions;
     }
 
+    /**
+     * Méthode toString de l'historique
+     * @return String représentant l'historique
+     */
     public String toString(){
         String affiche = "";
         for (String action : this.actions) {
