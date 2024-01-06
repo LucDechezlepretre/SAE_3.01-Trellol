43d8bb8dd6f05053061cfe773a55b96d26ff5242
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 19:13:12 +0100

    ajout de javadoc pour Importance

--- src/main/java/trellol/trellol/Importance.java
+++ src/main/java/trellol/trellol/Importance.java
@@ -1,9 +1,20 @@
 package trellol.trellol;
 
-import java.util.Map;
-
+/**
+ * Classe Importance servant à représenter tous les degrès d'importance
+ * possible pour une tâche
+ */
 public class Importance {
+    /**
+     * Attribut statique représentant une importance faible
+     */
     public static int FAIBLE = 0;
+    /**
+     * Attribut statique représentant une importance moyenne
+     */
     public static int MOYENNE = 1;
+    /**
+     * Attribut statique représentant une importance élevée
+     */
     public static int ELEVEE = 2;
 }
