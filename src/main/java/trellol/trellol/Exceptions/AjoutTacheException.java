8d005cea9d5c508705ec37d2d5c9f14acd48014d
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 13:56:30 +0100

    ajout de javadoc pour AjoutTacheException

--- src/main/java/trellol/trellol/Exceptions/AjoutTacheException.java
+++ src/main/java/trellol/trellol/Exceptions/AjoutTacheException.java
@@ -1,5 +1,9 @@
 package trellol.trellol.Exceptions;
 
+/**
+ * Classe AjoutTacheException héritant de la classe Exception,
+ * lancé pour un problème lors de l'ajout ou la modification d'une tache
+ */
 public class AjoutTacheException extends Exception{
     public AjoutTacheException(String msg){
         super(msg);
