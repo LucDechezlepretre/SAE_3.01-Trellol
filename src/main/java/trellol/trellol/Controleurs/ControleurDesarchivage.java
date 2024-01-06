3eae8f8bb2ac3746e4a62531e94a4fa682220e0d
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-06 14:47:23 +0100

    ajout de javadoc pour ControleurDesarchivage

--- src/main/java/trellol/trellol/Controleurs/ControleurDesarchivage.java
+++ src/main/java/trellol/trellol/Controleurs/ControleurDesarchivage.java
@@ -4,16 +4,33 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import trellol.trellol.Modele.Modele;
 import trellol.trellol.Tache;
-
+/**
+ * Classe controleur pour le desarchivage d'une tache
+ */
 public class ControleurDesarchivage implements EventHandler {
+    /**
+     * Attribut m de type Modele
+     */
     private Modele m;
+    /**
+     * Attribut tache de type Tache représentant la tâche à desarchiver
+     */
     private Tache tache;
 
+    /**
+     * Constructeur du controleur
+     * @param m modele auquel on desarchivera la tâche
+     * @param t tache à desarchiver
+     */
     public ControleurDesarchivage(Modele m, Tache t){
         this.m=m;
         this.tache=t;
     }
-
+    /**
+     * Redéfinition de la méthode handle, appelé lors du "clique"
+     * sur le bouton desarchiver depuis l'onglet "Archive"
+     * @param event évènement sur le bouton associé
+     */
     @Override
     public void handle(Event event) {
         this.m.desarchiverTache(tache);
