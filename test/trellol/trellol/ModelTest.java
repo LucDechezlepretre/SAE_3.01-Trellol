bb1a653f5e63f7556bb543392b477c37dc41d072
Lucoss <luc.dechezlepretre3@etu.univ-lorraine.fr>
2024-01-08 20:00:45 +0100

    ajout tests déplacer tâche

--- test/trellol/trellol/ModelTest.java
+++ test/trellol/trellol/ModelTest.java
@@ -102,7 +102,52 @@ class ModelTest {
         assertEquals(t, t2.getParent(),"La tache devrait etre déplacée");
     }
 
+    /**
+     * Vérification du blocage lors du déplacement d'une tâche dans une tâche dont le parent
+     * est antécédent de la tâche déplacé
+     * @throws AjoutTacheException
+     */
+    @Test
+    public void deplacerTacheParentAntecedent() throws AjoutTacheException{
+        Tache t = new Tache("tache1", "13/12/2023", 2, "enfant", 1);
+        Tache t2 = new Tache("tache2", "13/12/2023", 2, "parent", 1);
+        Tache t3 = new Tache("tache3", "13/12/2023", 2, "enfant2", 1);
 
+        m.ajouterTache(t);
+        t2.setParent(t);
+        t2.setAntecedant(t3);
+        m.ajouterTache(t2);
+        t3.setParent(t);
+        m.ajouterTache(t3);
+        m.deplacerTache(t3, t2);
+        assertEquals(t, t3.getParent(), "la tâche t3 devrait avoir la tâche t comme parent");
+    }
+
+    /**
+     * Vérification du blocage lors du déplacement d'une tâche dans une tâche dont l'un des parent
+     * est antécédent de la tâche déplacé
+     * @throws AjoutTacheException
+     */
+    @Test
+    public void deplacerTacheParentDeParentAntecedent() throws AjoutTacheException{
+        Tache t = new Tache("tache1", "13/12/2023", 2, "enfant", 1);
+        Tache t2 = new Tache("tache2", "13/12/2023", 2, "parent", 1);
+        Tache t3 = new Tache("tache3", "13/12/2023", 2, "enfant2", 1);
+        Tache t4 = new Tache("tache4", "14/12/2023", 2, "enfant3", 1);
+        m.ajouterTache(t);
+
+        t4.setParent(t);
+        t4.setAntecedant(t3);
+        m.ajouterTache(t4);
+
+        t2.setParent(t4);
+        m.ajouterTache(t2);
+
+        t3.setParent(t);
+        m.ajouterTache(t3);
+        m.deplacerTache(t3, t2);
+        assertEquals(t, t3.getParent(), "la tâche t3 devrait avoir la tâche t comme parent");
+    }
     /**
      * vérification de l'ajout d'une tache
      * Pour cela il faut que la tache soi la première tache (racine) ou bien qu'elle ai un parent pour pouvoir etre ajoutée
