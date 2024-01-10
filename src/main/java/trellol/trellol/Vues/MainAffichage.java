f9e6ae25d9007a789533932d317f37e83b82e571
despaqui9u <liamdespaqiis@gmail.com>
2024-01-10 10:55:37 +0100

    Diagramme Sequence Gantt

--- src/main/java/trellol/trellol/Vues/MainAffichage.java
+++ src/main/java/trellol/trellol/Vues/MainAffichage.java
@@ -162,28 +162,140 @@ public class MainAffichage extends Application {
         Tache racine = new Tache("racine", "13/12/2023", 15, "Ceci est la racine", Importance.FAIBLE);
         try {
             model.ajouterTache(racine);
-            Tache racine2 = new Tache("racine2", "14/12/2023", 2, "Ceci est la racine 2 ", Importance.MOYENNE);
-            racine2.setParent(racine);
-            model.ajouterTache(racine2);
+            Tache O = new Tache("O", "14/12/2023", 2, "Ceci est la tache O", Importance.MOYENNE);
+            O.setParent(racine);
+            model.ajouterTache(O);
 
-            Tache luc = new Tache("tacheLuc", "14/12/2023", 2, "Ceci n'est pas la racine", Importance.FAIBLE);
-            luc.setParent(racine);
-            model.ajouterTache(luc);
+            Tache O1 = new Tache("O1", "14/12/2023", 2, "Ceci est la tache O1", Importance.MOYENNE);
+            O1.setParent(O);
+            model.ajouterTache(O1);
 
-            Tache t2 = new Tache("tache2","18/12/2023", 2, "Ceci n'est pas la racine", Importance.ELEVEE);
-            t2.setParent(luc);
-            t2.setAntecedant(racine2);
-            model.ajouterTache(t2);
+            Tache O2 = new Tache("O2", "14/12/2023", 2, "Ceci est la tache O2", Importance.MOYENNE);
+            O2.setParent(O);
+            model.ajouterTache(O2);
 
-            Tache t3 = new Tache("tache3","17/12/2023", 2, "Ceci n'est pas la racine", 0);
-            t3.setParent(luc);
-            t3.setAntecedant(racine2);
-            model.ajouterTache(t3);
+            Tache O3 = new Tache("O3", "14/12/2023", 2, "Ceci est la tache O3", Importance.MOYENNE);
+            O3.setParent(O);
+            model.ajouterTache(O3);
 
-            Tache t4 = new Tache("tache4","17/12/2023", 2, "Ceci n'est pas la racine", 0);
-            t4.setParent(t3);
-            t4.setAntecedant(racine2);
-            model.ajouterTache(t4);
+            Tache P = new Tache("P", "14/12/2023", 2, "Ceci est la tache P", Importance.FAIBLE);
+            P.setParent(racine);
+            model.ajouterTache(P);
+
+            Tache P1 = new Tache("P1", "14/12/2023", 2, "Ceci est la tache P1", Importance.FAIBLE);
+            P1.setParent(P);
+            model.ajouterTache(P1);
+
+            Tache P2 = new Tache("P2", "14/12/2023", 2, "Ceci est la tache P2", Importance.FAIBLE);
+            P2.setParent(P);
+            model.ajouterTache(P2);
+
+            Tache P3 = new Tache("P3", "14/12/2023", 2, "Ceci est la tache P3", Importance.FAIBLE);
+            P3.setParent(P);
+            model.ajouterTache(P3);
+
+            Tache Q = new Tache("Q","18/12/2023", 2, "Ceci est la tache Q", Importance.ELEVEE);
+            Q.setParent(racine);
+            model.ajouterTache(Q);
+
+            Tache QD = new Tache("QD","18/12/2023", 2, "Ceci est la tache QD", Importance.ELEVEE);
+            QD.setParent(Q);
+            model.ajouterTache(QD);
+
+            Tache QD1 = new Tache("QD1","18/12/2023", 2, "Ceci est la tache QD1", Importance.ELEVEE);
+            QD1.setParent(QD);
+            model.ajouterTache(QD1);
+
+            Tache QD2 = new Tache("QD2","18/12/2023", 2, "Ceci est la tache QD2", Importance.ELEVEE);
+            QD2.setParent(QD);
+            model.ajouterTache(QD2);
+
+            Tache R = new Tache("R","17/12/2023", 2, "Ceci est la tache R", 0);
+            R.setParent(racine);
+            model.ajouterTache(R);
+
+            Tache R1 = new Tache("R1","17/12/2023", 2, "Ceci est la tache R1", 0);
+            R1.setParent(R);
+            model.ajouterTache(R1);
+
+            Tache R2 = new Tache("R2","17/12/2023", 2, "Ceci est la tache R2", 0);
+            R2.setParent(R);
+            model.ajouterTache(R2);
+
+            Tache R3 = new Tache("R3","17/12/2023", 2, "Ceci est la tache R3", 0);
+            R3.setParent(R);
+            model.ajouterTache(R3);
+
+            Tache T = new Tache("T","17/12/2023", 2, "Ceci est la tache T", 0);
+            T.setParent(racine);
+            model.ajouterTache(T);
+
+            Tache T1 = new Tache("T1","17/12/2023", 2, "Ceci est la tache T1", 0);
+            T1.setParent(T);
+            model.ajouterTache(T1);
+
+            Tache T2 = new Tache("T2","17/12/2023", 2, "Ceci est la tache T2", 0);
+            T2.setParent(T);
+            model.ajouterTache(T2);
+
+            Tache T3 = new Tache("T3","17/12/2023", 2, "Ceci est la tache T3", 0);
+            T3.setParent(T);
+            model.ajouterTache(T3);
+
+            Tache X = new Tache("U","17/12/2023", 2, "Ceci est la tache U", 0);
+            X.setParent(racine);
+            model.ajouterTache(X);
+
+            Tache Y = new Tache("Y","17/12/2023", 2, "Ceci est la tache Y", 0);
+            Y.setParent(racine);
+            model.ajouterTache(Y);
+
+            Tache Z = new Tache("Z","17/12/2023", 2, "Ceci est la tache Z", 0);
+            Z.setParent(racine);
+            model.ajouterTache(Z);
+
+            Tache D = new Tache("D","15/01/2023", 14, "Ceci est la tache D", 0);
+            D.setParent(X);
+            model.ajouterTache(D);
+
+            Tache D1 = new Tache("D1","17/12/2023", 2, "Ceci est la tache D1", 0);
+            D1.setParent(D);
+            model.ajouterTache(D1);
+
+            Tache C = new Tache("C","01/02/2023", 7, "Ceci est la tache C", 0);
+            C.setParent(X);
+            C.setAntecedant(D);
+            model.ajouterTache(C);
+
+            Tache A = new Tache("A","15/02/2023", 21, "Ceci est la tache A", 0);
+            A.setParent(X);
+            A.setAntecedant(C);
+            model.ajouterTache(A);
+
+            Tache A1 = new Tache("A1","18/02/2023", 7, "Ceci est la tache A1", 0);
+            A1.setParent(A);
+            model.ajouterTache(A1);
+
+            Tache A2 = new Tache("A2","27/02/2023", 7, "Ceci est la tache A2", 0);
+            A2.setParent(A);
+            model.ajouterTache(A2);
+
+            Tache B = new Tache("B","22/05/2023", 35, "Ceci est la tache B", 0);
+            B.setParent(X);
+            model.ajouterTache(B);
+
+            Tache B1 = new Tache("B1","13/03/2023", 14, "Ceci est la tache B1", 0);
+            B1.setParent(B);
+            model.ajouterTache(B1);
+
+            Tache B2 = new Tache("B2","12/03/2023", 7, "Ceci est la tache B2", 0);
+            B2.setParent(B);
+            B2.setAntecedant(A2);
+            model.ajouterTache(B2);
+
+            Tache B3 = new Tache("B3","05/03/2023", 21, "Ceci est la tache B3", 0);
+            B3.setParent(B);
+            model.ajouterTache(B3);
 
         }
         catch(AjoutTacheException e){
